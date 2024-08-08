e6c176c9a110767c8e43307f840f1b16be07b215
Aleksandar Bayrev <aleksandarbayrev@gmail.com>
2024-08-08 18:21:29 +0300

    Update context check

--- src/base/helpers/isValidContext.ts
+++ src/base/helpers/isValidContext.ts
@@ -1,4 +1,12 @@
 import { AppContext } from "../../AppContext";
 import { DependencyInjection } from "../DependencyInjection";
 
-export const isValidContext = (context: any): context is AppContext => context && context.dependencyInjection instanceof DependencyInjection;
+export const isValidContext = (context: any): context is AppContext => {
+    const contextChecks: boolean[] = [
+        typeof context !== "undefined",
+        context !== undefined,
+        context !== null,
+        context.dependencyInjection instanceof DependencyInjection
+    ];
+    return contextChecks.every(x => x);
+};
