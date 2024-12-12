51c09b27550fef6cbdbdf5b147d18f741d7a3354
Aleksandar Bayrev <aleksandarbayrev@gmail.com>
2024-12-12 23:59:47 +0200

    Fix loader

--- src/loader/index.ts
+++ src/loader/index.ts
@@ -1,5 +1,7 @@
 import '@app-styles/App.css';
-
-window.document.addEventListener("load", () => {
-    document.body.innerHTML = "Loading application, please wait...";
+document.body.addEventListener("load", () => {
+    const container = document.createElement('div');
+    container.classList.add('app-wrapper');
+    container.innerHTML = 'Loading website, please wait...';
+    document.body.appendChild(container);
 });
