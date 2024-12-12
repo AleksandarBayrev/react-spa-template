213b8e615d76596a7747fed4646c74a828b69e39
Aleksandar Bayrev <aleksandarbayrev@gmail.com>
2024-12-13 00:30:52 +0200

    Update pages

--- src/app/pages/AboutPage/AboutPage.tsx
+++ src/app/pages/AboutPage/AboutPage.tsx
@@ -1,7 +1,15 @@
+import { AppContext } from "@app-root/AppContext";
+import { AppContextProvider } from "@app-root/base";
+import { IAppStore } from "@app-root/interfaces";
 import { observer } from "mobx-react";
-import React from "react";
+import React, { useEffect } from "react";
 
 export const AboutPage = observer(() => {
+    const context = AppContextProvider.getContext(AppContext);
+    const appStore = context.DependencyInjection.getService<IAppStore>("IAppStore");
+    useEffect(() => {
+        console.log(appStore.appName);
+    }, []);
     return (
         <div>About page</div>
     )
--- src/app/pages/HomePage/HomePage.tsx
+++ src/app/pages/HomePage/HomePage.tsx
@@ -1,13 +1,21 @@
+import { AppContext } from "@app-root/AppContext";
+import { AppContextProvider } from "@app-root/base";
+import { IAppStore } from "@app-root/interfaces";
 import { observer } from "mobx-react";
-import React from "react";
+import React, { useEffect } from "react";
 import { useLocation, useParams } from "react-router-dom";
 
 
 export const HomePage = observer(() => {
+    const context = AppContextProvider.getContext(AppContext);
+    const appStore = context.DependencyInjection.getService<IAppStore>("IAppStore");
     const params = useParams();
     const location = useLocation();
     const searchParams = new URLSearchParams(location.search);
     const searchParamsArray = [...searchParams.entries()];
+    useEffect(() => {
+        console.log(appStore.appName);
+    }, []);
     return (
         <div>Home Page
             {searchParamsArray.length ? <div>, search params: {JSON.stringify(searchParamsArray)}</div> : ""}
